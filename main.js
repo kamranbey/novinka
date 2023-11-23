9b264bed5204d5a552908556c0cd66de3e50f6da
Kamran <kamranbey886@gmail.com>
2023-11-23 11:20:36 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('salam saleykym bratik');
+console.log('Çuvak Ti Geniy');
+
+let a = 'Sa';
+let b = 'ria';
+
+console.log(a + b);
+
+const ti = { a, b };
+
+let arrey = (1, 2, 3);
+
+console.log(typeof arrey);
